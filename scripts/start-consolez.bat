4f2b3554d6da19a4a9a83f655976c3b7ea5c27eb
Mikael Arneborn <mikael@arneborn.net>
2015-10-28 09:23:05 -0700

    made improvements to consolez startup scripts

--- scripts/start-consolez.bat
+++ scripts/start-consolez.bat
@@ -1,11 +1,50 @@
-start /b C:\Users\Mikael\Programming\Tools\ConsoleZ\Console.exe ^
- -t "Git bash" -d C:\Users\Mikael\Programming\meanTemplate ^
- -t PyCmd -d C:\Users\Mikael\Programming\meanTemplate -r C:\Users\Mikael\bin\emacs.bat
+@echo off
 
-start /b C:\Users\Mikael\Programming\Tools\ConsoleZ\Console.exe ^
- -t DB -d C:\Users\Mikael\Programming\mongoDBs\common ^
- -t server  -d C:\Users\Mikael\Programming\meanTemplate ^
- -t browser -d C:\Users\Mikael\Programming\meanTemplate ^
- -t jshint -d C:\Users\Mikael\Programming\meanTemplate ^
- -t test -d C:\Users\Mikael\Programming\meanTemplate
+SET DIRNAME=%~dp0
 
+REM Make sure %MONGODB% is set
+IF NOT DEFINED MONGODB ( 
+    set MONGODB=%USERPROFILE%\Programming\mongoDBs\common
+)
+
+IF NOT EXIST %MONGODB% (
+  echo This mongodb doesn't exist: %MONGODB%
+  GOTO END
+)
+
+REM Make sure we know where ConsoleZ livs
+IF NOT DEFINED CONSOLEZ (
+   set CONSOLEZ=C:\Users\Mikael\Programming\Tools\ConsoleZ\Console.exe
+)
+
+echo Running in: %CD%
+REM Make sure %WORK% is set
+IF NOT DEFINED WORK (
+    set WORK=%CD%
+)
+
+IF NOT EXIST %WORK% (
+  echo This WORK doesn't exist: %WORK%
+  GOTO END
+)
+
+REM - give the DB enough time to start befor launching servers
+REM - give the servers enough time to start before launching the tests, jshint, and browser
+REM - FIXME - need to add jshint
+REM - FIXME - need to finish test
+
+IF EXIST "%MONGODB%\mongod.lock" (
+   echo That mongoDB is already running or has crashed.
+   echo Do nothing and this app will use the running one
+   echo If the db has crashed try this:
+   echo rm %MONGODB%\mongod.lock
+   echo mongod --dbpath %MONGODB%
+)
+
+START /MIN %DIRNAME%start-consolez-bg.bat
+START /MIN %DIRNAME%start-consolez-int.bat
+START /b emacs
+
+:END
+
+exit
