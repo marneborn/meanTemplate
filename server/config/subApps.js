80f665b8d7251b2f34bddbc817e591c86db06c2a
Mikael Arneborn <mikael@arneborn.net>
2016-03-14 15:54:55 -0700

    Moved header into web/components

--- server/config/subApps.js
+++ server/config/subApps.js
@@ -1,9 +1,14 @@
 "use strict";
 
-module.exports = {
+let config = module.exports = {
     list : [
         'testapp1',
         'testapp2'
     ],
     default : 'testapp1'
 };
+
+for (let i=0; i<config.list.length; i++) {
+    let subAppName = config.list[i];
+    config[subAppName] = require('../'+subAppName+'/config');
+}
